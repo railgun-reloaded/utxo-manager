6051b636abd8f11ffe50f70c38d96507394d9859
bhflm <germanbonin@gmail.com>
2026-02-18 09:36:09 -0700

    chore: remove comment

--- src/spend/index.ts
+++ src/spend/index.ts
@@ -1,16 +1,2 @@
 export * from './models'
 export * from './solution'
-
-/*
-  so we generate spend intents, from there the solver is passed these, and utxo's to attempt to fulfill the intent.
-  each railgun transaction can have a single token, but many outputs(recipients). ie max ratio input + output = 14.
-  
-  so this means we can have a number of different spending solutions.
-
-
-  ideally we would like to create a transaction with the lowest inputs/outputs but this might not be solvable,
-  need to potentially break down into many railgun transactions. ie: max inputs filled for dest + change (2) 
-  and we sill have X+n recipients to add. 
-
-  
-*/
