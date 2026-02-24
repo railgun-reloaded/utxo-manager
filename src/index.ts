687c21aac23500b6bd7517d7f2d3ea46472f2900
bhflm <germanbonin@gmail.com>
2026-02-24 06:39:15 -0700

    fix: correct exports

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,14 @@ import { RailgunSolver } from './spend/solution'
 export * from './solver'
 export * from './models'
 export * from './spend'
+
 export type { UTXOSolver, SolveParams, SolveResult, GreedySolveParams, RailgunSolveParams } from './interfaces'
+export { SolverKind } from './interfaces'
 export { BaseSolver } from './base-solver'
-export { GreedySolver } from './solver'
-export { RailgunSolver } from './spend/solution'
 export { VALID_INPUT_COUNTS, VALID_OUTPUT_COUNTS } from './solutions/nullifiers'
 export { selectInputsForTarget, findExactMatch } from './solutions/selection'
 export { sortUTXOsByAscendingValue, sortUTXOsByDescendingValue } from './solutions/utxos'
 
-/**
- * Create a solver instance by type.
- * @param type - Solver type
- * @returns Solver instance
- */
 export function createSolver (type: 'greedy' | 'railgun' = 'railgun'): UTXOSolver {
   return type === 'greedy' ? new GreedySolver() : new RailgunSolver()
 }
